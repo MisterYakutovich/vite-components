0eb0eecfeaf3022abb9eeb0becf4e38ba60e574d
Pavel <pashtet.net5@gmail.com>
2023-10-29 14:44:13 +0300

    feat:add components:Main,Seach,Page;add css for components;add API for displaying cards and function for displaying cards if enter button search

---
+++ src/Page.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Seach from './components/Seach';
+import Main from './components/Main';
+
+interface PageBeers {
+  show: string;
+  beer: Beer[];
+  movie: Beer;
+  loading: boolean;
+}
+interface Beer {
+  
+}
+class Page extends React.Component<Record<string, never>, PageBeers> {
+  state:PageBeers = {
+    show: 'index',
+    beer: [],
+    movie: {},
+    loading: true,
+}
+handleEnter = (search:string):void => {
+  if (search.trim() === "") return;
+  this.setState({
+      loading: true,
+      show: 'search'
+  });
+  search = encodeURIComponent(search);
+  const url = `https://api.punkapi.com/v2/beers?brewed_before=11-2012&beer_name=${search}`;
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          this.setState({
+              beer: data, 
+              loading: false
+          });
+      });
+}
+  render(){
+    return(
+    <>
+       <Seach enterHandler={this.handleEnter}/>
+       <Main searchName={this.state.beer}/>       
+    </>
+    )
+}
+}
+export default Page
---
+++ src/components/Main.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Main.css";
+
+interface SearchState {
+  search: string;
+  items: string[];
+  isLoaded: boolean;
+  //error: any;
+}
+interface BeerProps {
+  searchName:string[]
+}
+interface BeersArray {
+  name:string;
+  image_url:string;
+  id:string;
+}
+interface BeersSearch {
+  name:string;
+  image_url:string;
+  id:string;
+}
+class Main extends React.Component<BeerProps, SearchState> {
+  
+  state: SearchState = {
+    search: "",
+    items: [],
+    isLoaded: false,
+  }   
+    componentDidMount() {
+        fetch("https://api.punkapi.com/v2/beers?page=2&per_page=10")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              console.log(result)
+              this.setState({
+                isLoaded: true,
+                items: result             
+              });
+            },           
+            () => {
+              this.setState({
+                isLoaded: true,
+              });
+            }            
+          )
+      } 
+   render() {
+
+const arr:BeersArray[]=this.state.items;
+const arrSearch:BeersArray[]=this.props.searchName;
+
+     return(     
+      <section className="section-main">
+       <div className="container">
+      { this.props.searchName.length==0 ?(
+       arr.map((i)=> (
+        <div key={i.id} className="card">
+          <img className="card_img" key={i.image_url} src={i.image_url} alt=""></img>
+          <p key={i.name} className="card_title">{i.name}</p>
+        </div>  
+       ))):(
+        arrSearch.map((i)=> (
+          <div key={i.id} className="card">
+            <img className="card_img" key={i.image_url} src={i.image_url} alt=""></img>
+            <p key={i.name} className="card_title">{i.name}</p>
+          </div>  
+       
+       )))}
+       </div>
+      </section>
+        )
+    }        
+    }
+export default Main;
